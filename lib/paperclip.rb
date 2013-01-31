23bf97d9f70a27a62c19c92891c2c83b12918de1
Saravana K <saravk78@gmail.com>
2013-02-01 03:08:40 +0530

    update class_attribute

--- lib/paperclip.rb
+++ lib/paperclip.rb
@@ -341,7 +341,7 @@ module Paperclip
     # Returns the attachment definitions defined by each call to
     # has_attached_file.
     def attachment_definitions
-      read_inheritable_attribute(:attachment_definitions)
+      class_attribute(:attachment_definitions)
     end
   end
 
