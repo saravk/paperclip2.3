99486b8e0a3c5dcdc216c12e6a6f9e5d00f6a794
Saravana K <saravk78@gmail.com>
2013-02-01 03:23:07 +0530

    update class_attribute

--- lib/paperclip.rb
+++ lib/paperclip.rb
@@ -235,7 +235,12 @@ module Paperclip
     def has_attached_file name, options = {}
       include InstanceMethods
 
-      class_attibute(:attachment_definitions, {}) if attachment_definitions.nil?
+      if attachment_definitions.nil?
+        self.attachment_definitions = {}
+      else
+        self.attachment_definitions = self.attachment_definitions.dup
+      end
+
       attachment_definitions[name] = {:validations => []}.merge(options)
 
       after_save :save_attached_files
@@ -340,9 +345,9 @@ module Paperclip
 
     # Returns the attachment definitions defined by each call to
     # has_attached_file.
-    def attachment_definitions
-      class_attribute(:attachment_definitions)
-    end
+    # def attachment_definitions
+    #   class_attribute(:attachment_definitions)
+    # end
   end
 
   module InstanceMethods #:nodoc:
